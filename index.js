efcdda493316c39eb56c04d56dd8132d1317ea56
Abrar <hossainabrar@aol.com>
2024-02-02 02:15:35 -0500

    parse through each crypto

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => fetchData())
+
+// fetch data from API and convert to json
 function fetchData(){
     fetch('https://api.coincap.io/v2/assets')
     .then(resp => resp.json())
-    .then(data => console.log(data))
+    .then(dataSet => handleData(dataSet))
+}
+
+
+function handleData(dataSet){
+    //console.log(data.data[1].id)
+    dataSet.data.forEach(element => {
+        console.log(element)
+    })
 }
