c9c15944bbb7e15686390ffe2a0ab11bfface9f6
Abrar <hossainabrar@aol.com>
2024-02-02 02:34:04 -0500

    populated dropdown

--- index.js
+++ index.js
@@ -10,7 +10,21 @@ function fetchData(){
 
 function handleData(dataSet){
     //console.log(data.data[1].id)
+    // Create a select html element to create dropwdown
+    let sel = document.createElement('select')
+    document.querySelector('body').appendChild(sel)
+    // Create a default option for the dropdown list
+    let defaultOption = document.createElement('option');
+    defaultOption.textContent = "Choose Crypto"; // Set the default text
+    defaultOption.disabled = true; // Make it disabled so it's not selectable
+    defaultOption.selected = true; // Make it the initially selected option
+    // Append the default option to the select element
+    sel.appendChild(defaultOption);
+    // Create a dropdown option list for each crypto
     dataSet.data.forEach(element => {
-        console.log(element)
+        let opt = document.createElement('option')
+        opt.textContent = element.id
+        sel.appendChild(opt)
+
     })
 }
